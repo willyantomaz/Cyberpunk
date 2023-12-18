9276bc21f3a6c1c855a64b0af48d95466d350001
willyantomaz <willyantomaz2015@gmail.com>
2023-12-18 13:54:27 -0300

    Initial commit

---
+++ src/main/java/com/projeto/inicial/CyberpunkApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.inicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberpunkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberpunkApplication.class, args);
+	}
+
+}
