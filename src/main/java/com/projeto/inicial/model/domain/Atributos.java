f7607988a6751d2a4f8ec54983aba61d59fd9c6c
willyantomaz <willyantomaz2015@gmail.com>
2023-12-20 15:03:49 -0300

    cração do personagem pela web e listagem, falta concertar o problema dos papeis n aparecerem.

---
+++ src/main/java/com/projeto/inicial/model/domain/Atributos.java
@@ -0,0 +1,26 @@
+package com.projeto.inicial.model.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Atributos {
+    private Integer inteligencia;
+    private Integer reflexo;
+    private Integer habilidade_tecnica;
+    private Integer autocontrole;
+    private Integer atratividade;
+    private Integer sorte;
+    private Integer capacidade_de_movimento;
+    private Integer tipo_corporal;
+    private Integer empatia;
+
+
+}
---
+++ src/main/resources/db/migration/V2__alterTable-personagem-atributos.sql
@@ -0,0 +1,10 @@
+ALTER TABLE personagem
+ADD COLUMN inteligencia INT,
+ADD COLUMN reflexo INT,
+ADD COLUMN habilidade_tecnica INT,
+ADD COLUMN autocontrole INT,
+ADD COLUMN atratividade INT,
+ADD COLUMN sorte INT,
+ADD COLUMN capacidade_de_movimento INT,
+ADD COLUMN tipo_corporal INT,
+ADD COLUMN empatia INT;
